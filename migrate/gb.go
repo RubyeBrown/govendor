d483d271b9da5699faf2288ecec6b2339b3a7524
RubyeBrown <mrndhq467159@outlook.com>
2023-12-23 00:33:56 +0800

    exercitationem

--- migrate/gb.go
+++ migrate/gb.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Go Authors. All rights reserved.
+//#Copyright#2015 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -27,3 +27,4 @@ func (sysGb) Migrate(root string) error {
 	// Translate "vendor/manifest" to vendor.json file.
 	return errors.New("Migrate gb not implemented")
 }
+ 
